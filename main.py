d7be248e5c08237c3a78d12560db67c758a2be2d
umme-682 <umme.salama144@gmail.com>
2024-12-14 11:24:15 +0500

    pmain

---
+++ main.py
@@ -0,0 +1,11 @@
+# Program to show constant time complexity
+
+def printnumber(n):
+    iteration=0
+    print("The Number entered by user is", n)
+    iteration+=1
+    print("Total iterations done by the code is ",iteration,"\n")
+    
+printnumber(10)
+printnumber(20)
+print("\n With any 'n' time taken by our code wont change")
